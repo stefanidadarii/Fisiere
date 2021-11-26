1b5456d6a7f43865974bc4c8bb9092e6c350498f
stefanidadarii <70660311+stefanidadarii@users.noreply.github.com>
2021-11-26 17:03:57 +0200

    Update Problema_5 p 77.py

--- Problema_5 p 77.py
+++ Problema_5 p 77.py
@@ -1,9 +1,10 @@
 with open ('c:/Users/User/Desktop/input.txt', 'r') as f:
-    C=f.readline()
-numar_vocale=0
+    V=f.readline()
+nr_vocale=0
+vocale='aeiouAEIOU'
 with open('c:/Users/User/Desktop/output.txt', 'w') as f:
-    for i in C:
-        if i in 'aeiouAEIOU':
+    for i in V:
+        if i in vocale:
             f.write("Vocalele sunt:"+i)
-            numar_vocale+=1
+            nr_vocale+=1
 print('Numarul vocalelor este:', numar_vocale)
