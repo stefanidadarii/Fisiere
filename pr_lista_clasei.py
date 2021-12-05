efaf4c2e38ea7e79bafe7210b6524f53ce800058
stefanidadarii <70660311+stefanidadarii@users.noreply.github.com>
2021-12-05 16:25:56 +0200

    Add files via upload

--- pr_lista_clasei.py
+++ pr_lista_clasei.py
@@ -1,18 +1,10 @@
-with open('c:/Users/User/Desktop/Lista Clasei 11A.txt', 'r') as f:
+with open('c:/Users/User/Desktop/ListaClasei11A.txt', 'r') as f:
     elevi=list(f)
 n=media=0
-print('Nume','Prenume','Nota','Limba',sep='\t')
-a=open('c:/Users/User/Desktop/Elevii_Ce_Studiaza_LimbaEngleza.txt', 'w')
-b=open('c:/Users/User/Desktop/Elevii_Ce_Studiaza_LimbaGermana.txt', 'w')
+print('Nume','Prenume','Nota',sep='\t')
 for i in elevi:
-    elev = i.split()
+    elev=i.split()
     nota=int(elev[2])
     n,media=n+1,media+nota
-    print(elev[0],elev[1],nota,elev[3], sep='\t')
-    if elev[3].lower() == 'engleza':
-        a.write(elev[0]+elev[1]+str(nota)+'\n')
-    elif elev[3].lower() == 'germana':
-        b.write(elev[0]+elev[1]+str(nota)+'\n')
-print('Media celor', n, 'elevi este', media/float(n))
-a.close()
-b.close()
+    print(elev[0],elev[1],nota,sep='\t')
+print('Media celor', n, 'elevi este', sum(media)/float(n))
